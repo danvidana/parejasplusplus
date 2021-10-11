a89a9f59d125fea7872e81679d994b23f70e141f
DanVidana <danvidana98@gmail.com>
2021-10-10 22:39:39 -0500

    TOKENS and Regular Expressions fix

--- compilador.py
+++ compilador.py
@@ -1,32 +1,115 @@
 import lex
 
 tokens = (
-    "IF",
-    "ELSE",
-    "FOR",
+    "ID",
+    "NUMBER",
+    "SEMICOLON",
+    "COMMA",
+    "PROGRAMA",
+    "VARS",
+    "VAR",
+    "MODULE",
     "INT",
-    "STRING",
+    "FLOAT",
+    "BOOLEAN",
+    "VOID",
+    "OPEN_PAREN",
+    "CLOSE_PAREN",
+    "OPEN_BRACKETS",
+    "CLOSE_BRACKETS",
+    "OPEN_BRACES",
+    "CLOSE_BRACES",
+    "EQUALS",
+    "PLUS",
+    "MINUS",
+    "MULTIPLIES",
+    "DIVIDES",
+    "LESS_THAN",
+    "MORE_THAN",
+    "IF",
+    "THEN",
+    "ELSE",
     "WHILE",
-    "SYMBOL",
-    "COUNT",
-    "IDENTIFIER"
+    "DO",
+    "FOR",
+    "TO",
+    "READ",
+    "WRITE",
+    "MAIN",
+    "RETURN"
 )
 
-t_IDENTIFIER = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
-t_IF = r"if"
+t_NUMBER = r'[0-9]+'
 
-t_ELSE = r"else"
+t_SEMICOLON = r";"
 
-t_FOR = r"for"
+t_COMMA = r","
+
+t_PROGRAM = r"program"
+
+t_VARS = r"vars"
+
+t_VAR = r"var"
+
+t_MODULE = r"module"
 
 t_INT = r"int"
 
-t_STRING = r"string"
+t_FLOAT = r"float"
+
+t_BOOLEAN = r"bool"
+
+t_VOID = r"void"
+
+t_OPEN_PAREN = r"("
+
+t_CLOSE_PAREN = r"("
+
+t_OPEN_BRACKETS = r"["
+
+t_OPEN_BRACKETS = r"]"
+
+t_OPEN_BRACES = r"{"
+
+t_CLOSE_BRACES = r"}"
+
+t_EQUALS = r"="
+
+t_PLUS = r"+"
+
+t_MINUS = r"-"
+
+t_MULTIPLIES = r"*"
+
+t_DIVIDES = r"/"
+
+t_LESS_THAN = r"<"
+
+t_MORE_THAN = r">"
+
+t_IF = r"if"
+
+t_THEN = r"then"
+
+t_ELSE = r"else"
 
 t_WHILE = r"while"
 
+t_DO = r"do"
 
+t_FOR = r"for"
+
+t_TO = r"to"
+
+t_READ = r"read"
+
+t_WRITE = r"write"
+
+t_MAIN = r"main"
+
+t_RETURN = r"return"
 
 t_ignore = ' \t'
 
