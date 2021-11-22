9f1dde1f2c1fdec1f05190eb98b0bb2cbcd957c0
Graze <danvidana98@gmail.com>
2021-11-21 22:54:24 -0600

    Syntax works 100%, start of neural points

--- parser.py
+++ parser.py
@@ -2,9 +2,23 @@ import sys
 import ply.yacc as yacc
 from scanner import tokens
 
+# Global variables (directories, tables)
+
+dirFunc = {}
+
+currentId = ''
+currentType = ''
+currentFunc = ''
+
+funcName = 'global'
+
 # function for program
 def p_program(p):
     'program : PROGRAM ID SEMICOLON vars funcs main'
+    # Create dirFunc
+    global dirFunc
+    dirFunc["program"]["name"] = p[2]
+    dirFunc["program"]["type"] = 'void'
     
     
 # funciton for main
@@ -13,13 +27,26 @@ def p_main(p):
 
 # function for funcs
 def p_funcs(p):
-    '''funcs : func_type MODULE ID OPEN_PAREN var_type ID CLOSE_PAREN vars block
+    '''funcs : func_type MODULE ID OPEN_PAREN funcs_params CLOSE_PAREN vars block
     | empty
     '''
 
+# function for funcs_complementary
 def p_funcs_comp(p):
-    '''funcs_comp : ID OPEN_PAREN var_type ID CLOSE_PAREN vars block
+    'funcs_comp : ID OPEN_PAREN funcs_params CLOSE_PAREN vars block'
+
+# function for funcs_params
+def p_funcs_params(p):
+    '''funcs_params : var_type variable funcs_params_comp
+    | empty
     '''
+
+# function for funcs_params_complementary
+def p_funcs_params_comp(p):
+    '''funcs_params_comp : COMMA var_type ID funcs_params_comp
+    | empty
+    '''
+
 
 # function for block
 def p_block(p):
@@ -40,18 +67,25 @@ def p_var_type(p):
 
 # function for vars
 def p_vars(p):
-    '''vars : VARS var_comp
+    '''vars : VARS create_var_table var_comp
     | empty
     '''
+
+def p_create_var_table(p):
+    '''create_var_table : '''
+    dirFunc[funcName] = {'vars':None}
+    if(dirFunc[funcName]['vars'] == None):
+        print("Sin tabla")
+    
     
 # function for var_comp, comp refers to complement
 def p_var_comp(p):
-    '''var_comp : var_type ids var_comp_2 var_comp_final
-    | var_type ids var_comp_2 SEMICOLON var_comp_recursive
+    '''var_comp : var_type ids_dec var_comp_2 var_comp_final
+    | var_type ids_dec var_comp_2 SEMICOLON var_comp_recursive
     '''
     
 def p_var_comp_2(p):
-    '''var_comp_2 : COMMA ids var_comp_3
+    '''var_comp_2 : COMMA ids_dec var_comp_3
     | empty
     '''
 
@@ -59,8 +93,8 @@ def p_var_comp_3(p):
     'var_comp_3 : var_comp_2'
 
 def p_var_comp_recursive(p):
-    '''var_comp_recursive : var_type ids var_comp_2 var_comp_final
-    | var_type ids var_comp_2 SEMICOLON var_comp_recursive
+    '''var_comp_recursive : var_type ids_dec var_comp_2 var_comp_final
+    | var_type ids_dec var_comp_2 SEMICOLON var_comp_recursive
     '''
 
 def p_var_comp_final(p):
@@ -73,31 +107,47 @@ def p_var_module_trans(p):
     '''var_module_trans : SEMICOLON var_type MODULE funcs_comp
     '''
 
-# function for ids
-def p_ids(p):
-    '''ids : ID OPEN_BRACKETS CT_INT CLOSE_BRACKETS OPEN_BRACKETS CT_INT CLOSE_BRACKETS
+# function for ids declarations
+def p_ids_dec(p):
+    '''ids_dec : ID OPEN_BRACKETS CT_INT CLOSE_BRACKETS OPEN_BRACKETS CT_INT CLOSE_BRACKETS
     | ID OPEN_BRACKETS CT_INT CLOSE_BRACKETS
     | ID
     '''
 
+# function for ids
+def p_ids(p):
+    '''ids : ID OPEN_BRACKETS exp CLOSE_BRACKETS OPEN_BRACKETS exp CLOSE_BRACKETS
+    | ID OPEN_BRACKETS exp CLOSE_BRACKETS
+    | ID
+    '''
+
+# Insers ID variable into symbol table
+# def insert_id(p):
+#     'insert_id : '
+#     global currentId, currentType, currentFunc
+#     currentId = p[-1]
+#     if context == 'global':
+#         d
+
+
 # function for statements
 def p_statements(p):
-    '''statements : assignment statements
-    | read statements
-    | write statements
+    '''statements : assignment SEMICOLON statements
+    | read SEMICOLON statements 
+    | write SEMICOLON statements
     | condition statements
-    | return statements
-    | func_call statements
+    | return SEMICOLON statements
+    | func_call SEMICOLON statements
     | empty
     '''
 
 # function for assignments
 def p_assignment(p):
-    'assignment : ids ASSIGN expressions SEMICOLON'
+    'assignment : ids ASSIGN expressions'
 
 # function for reads
 def p_read(p):
-    'read : READ OPEN_PAREN ids read_comp CLOSE_PAREN SEMICOLON'
+    'read : READ OPEN_PAREN ids read_comp CLOSE_PAREN'
 
 # function for read_complementary
 def p_read_comp(p):
@@ -123,79 +173,101 @@ def p_condition(p):
     '''condition : IF OPEN_PAREN expressions CLOSE_PAREN THEN block ELSE block
     | IF OPEN_PAREN expressions CLOSE_PAREN THEN block
     | WHILE OPEN_PAREN expressions CLOSE_PAREN DO block
-    | FOR ids ASSIGN expressions TO expressions DO block
+    | FOR ids_dec ASSIGN expressions TO expressions DO block
     '''
 
 # function for return
 def p_return(p):
-    'return : RETURN OPEN_PAREN exp CLOSE_PAREN SEMICOLON'
+    'return : RETURN OPEN_PAREN exp CLOSE_PAREN'
 
 # funciton for func_call
 def p_func_call(p):
-    'func_call : ID OPEN_PAREN func_call_comp CLOSE_PAREN SEMICOLON'
+    '''func_call : ID OPEN_PAREN func_call_comp CLOSE_PAREN
+    '''
 
 # function for func_call_complementary
 def p_func_call_comp(p):
-    '''func_call_comp : ID func_call_comp
-    | COMMA ID func_call_comp
-    | expressions
+    '''func_call_comp : expressions func_call_comp
+    | COMMA expressions func_call_comp
+    | empty
     '''
 
 # function for expressions
 def p_expressions(p):
-    '''expressions : exp expressions_op exp
-    | exp
+    'expressions : expressions_comp'
+
+# function for expressions_comp
+def p_expressions_comp(p):
+    '''expressions_comp : expression_comp_2
+    | expression_comp_2 OR expressions_comp
+    '''
+
+# function for expressions_comp_2
+def p_expression_comp_2(p):
+    '''expression_comp_2 : expression_comp_3
+    | expression_comp_3 AND expression_comp_2
+    '''
+
+# function for expressions_comp_3
+def p_expression_comp_3(p):
+    '''expression_comp_3 : exp
+    | exp expressions_op exp
     '''
 
 # function for expression operators
 def p_expressions_op(p): 
     '''expressions_op : LESS_THAN
+    | LESS_THAN_EQUAL
     | MORE_THAN
+    | MORE_THAN_EQUAL
     | EQUALS
     | NOT_EQUALS
-    | AND
-    | OR
     '''
 
 # function for exp
 def p_exp(p):
-    'exp : term exp_comp'
+    '''exp : term
+    | term exp_comp
+    '''
 
-# function for exp_complementary
+# function for exp_complementary (Sums and subtractions)
 def p_exp_comp(p):
     '''exp_comp : PLUS exp
     | MINUS exp
-    | empty
     '''
 
 # function for term
 def p_term(p):
-    'term : factor term_comp'
+    '''term : factor 
+    | factor term_comp
+    '''
 
-# function for term_complimentary
+# function for term_complimentary (Multiplications and divisions)
 def p_term_comp(p):
     '''term_comp : MULTIPLIES term
     | DIVIDE term
-    | empty
     '''
 
 # function for factor 
 def p_factor(p):
     '''factor : OPEN_PAREN expressions CLOSE_PAREN
-    | factor_comp ID
-    | factor_comp func_call
-    | ID
+    | variable
     | func_call
     | CT_INT
     | CT_FLOAT
     | CT_CHAR
     '''
 
-# function for factor_complimentary
-def p_factor_comp(p):
-    '''factor_comp : PLUS
-    | MINUS
+# function for variable
+def p_variable(p):
+    '''variable : ID
+    | ID dim
     '''
+
+# function for variable
+def p_dim(p):
+    'dim : OPEN_BRACKETS exp CLOSE_BRACKETS'
+
 
 # function for empty
 def p_empty(p):
