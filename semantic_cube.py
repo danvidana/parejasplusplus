0a355a9a2de2dfd9ff47aa19c715a9a37149ab63
danvidana <32367237+danvidana@users.noreply.github.com>
2021-11-24 23:48:30 -0600

    More options for constants

--- semantic_cube.py
+++ semantic_cube.py
@@ -86,7 +86,6 @@ semantic_cube['char']['==']['char'] = 'bool'
 semantic_cube['char']['!=']['char'] = 'bool'
 
 
-
 #CT_INT
 semantic_cube['ct_int']['+']['ct_int'] = 'int'
 semantic_cube['ct_int']['-']['ct_int'] = 'int'
@@ -130,8 +129,6 @@ semantic_cube['int']['<=']['ct_int'] = 'bool'
 #semantic_cube['int']['&&']['int'] = 'bool'
 #semantic_cube['int']['||']['int'] = 'bool'
 
-
-
 #INT - FLOAT
 semantic_cube['ct_int']['+']['ct_float'] = 'float'
 semantic_cube['ct_int']['-']['ct_float'] = 'float'
@@ -162,6 +159,34 @@ semantic_cube['ct_float']['<=']['ct_float'] = 'bool'
 #semantic_cube['float']['&&']['float'] = 'bool'
 #semantic_cube['float']['||']['float'] = 'bool'
 
+semantic_cube['float']['+']['ct_float'] = 'float'
+semantic_cube['float']['-']['ct_float'] = 'float'
+semantic_cube['float']['*']['ct_float'] = 'float'
+semantic_cube['float']['/']['ct_float'] = 'float'
+semantic_cube['float']['=']['ct_float'] = 'float'
+semantic_cube['float']['==']['ct_float'] = 'bool'
+semantic_cube['float']['!=']['ct_float'] = 'bool'
+semantic_cube['float']['>']['ct_float'] = 'bool'
+semantic_cube['float']['>=']['ct_float'] = 'bool'
+semantic_cube['float']['<']['ct_float'] = 'bool'
+semantic_cube['float']['<=']['ct_float'] = 'bool'
+#semantic_cube['float']['&&']['float'] = 'bool'
+#semantic_cube['float']['||']['float'] = 'bool'
+
+semantic_cube['ct_float']['+']['float'] = 'float'
+semantic_cube['ct_float']['-']['float'] = 'float'
+semantic_cube['ct_float']['*']['float'] = 'float'
+semantic_cube['ct_float']['/']['float'] = 'float'
+semantic_cube['ct_float']['=']['float'] = 'float'
+semantic_cube['ct_float']['==']['float'] = 'bool'
+semantic_cube['ct_float']['!=']['float'] = 'bool'
+semantic_cube['ct_float']['>']['float'] = 'bool'
+semantic_cube['ct_float']['>=']['float'] = 'bool'
+semantic_cube['ct_float']['<']['float'] = 'bool'
+semantic_cube['ct_float']['<=']['float'] = 'bool'
+#semantic_cube['float']['&&']['float'] = 'bool'
+#semantic_cube['float']['||']['float'] = 'bool'
+
 #FLOAT - INT
 semantic_cube['ct_float']['+']['ct_int'] = 'float'
 semantic_cube['ct_float']['-']['ct_int'] = 'float'
@@ -177,7 +202,29 @@ semantic_cube['ct_float']['<=']['ct_int'] = 'bool'
 #semantic_cube['float']['&&']['int'] = 'bool'
 #semantic_cube['float']['||']['int'] = 'bool'
 
+semantic_cube['ct_float']['+']['int'] = 'float'
+semantic_cube['ct_float']['-']['int'] = 'float'
+semantic_cube['ct_float']['*']['int'] = 'float'
+semantic_cube['ct_float']['/']['int'] = 'float'
+semantic_cube['ct_float']['=']['int'] = 'float'
+#semantic_cube['float']['==']['int'] = 'int'
+#semantic_cube['float']['!=']['int'] = 'int'
+semantic_cube['ct_float']['>']['int'] = 'bool'
+semantic_cube['ct_float']['>=']['int'] = 'bool'
+semantic_cube['ct_float']['<']['int'] = 'bool'
+semantic_cube['ct_float']['<=']['int'] = 'bool'
+#semantic_cube['float']['&&']['int'] = 'bool'
+#semantic_cube['float']['||']['int'] = 'bool'
+
 #CHAR
 semantic_cube['ct_char']['=']['ct_char'] = 'char'
 semantic_cube['ct_char']['==']['ct_char'] = 'bool'
 semantic_cube['ct_char']['!=']['ct_char'] = 'bool'
+
+semantic_cube['ct_char']['=']['char'] = 'char'
+semantic_cube['ct_char']['==']['char'] = 'bool'
+semantic_cube['ct_char']['!=']['char'] = 'bool'
+
+semantic_cube['char']['=']['ct_char'] = 'char'
+semantic_cube['char']['==']['ct_char'] = 'bool'
+semantic_cube['char']['!=']['ct_char'] = 'bool'
